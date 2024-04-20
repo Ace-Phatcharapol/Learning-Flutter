1c281a445d0e2d817e3c6b4c19d311b61494ffac
Ace-Phatcharapol <Phatcharapol2@gmail.com>
2024-04-20 23:21:20 +0700

    Use Expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "My App",
       home: MyHomePage(),
-      theme: ThemeData(primarySwatch: Colors.orange),
+      theme: ThemeData(primarySwatch: Colors.purple),
     );
   }
 }
@@ -26,7 +27,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("New Project"),
+          title: const Text(
+            "บัญชีของฉัน",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+          ),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -35,26 +39,89 @@ class _MyHomePageState extends State<MyHomePage> {
               Container(
                 padding: const EdgeInsets.all(8.0),
                 decoration: BoxDecoration(
-                    color: Colors.green, borderRadius: BorderRadius.circular(10)),
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(10)),
                 height: 100,
                 child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("Hello Flutter "),
-                    Text("Hello Dart"),
+                    Text(
+                      "ยอดคงเหลือ ",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "30000",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
                   ],
                 ),
               ),
               Container(
+                padding: const EdgeInsets.all(8.0),
                 decoration: BoxDecoration(
-                    color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-                height: 100,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.yellow,
+                    color: Colors.green,
                     borderRadius: BorderRadius.circular(10)),
                 height: 100,
-              )
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "รายรับ ",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "60000",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(10)),
+                height: 100,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "รายจ่าย ",
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "30000",
+                        style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700),
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ));
